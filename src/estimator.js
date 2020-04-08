32579099e378e148e8773333b7ae4dd42682d95e
courage173 <courageosemwengie@gmail.com>
2020-04-08 15:00:08 +0100

    completed challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,32 +1,26 @@
-import {
-    convertToDays,
-    caseReported,
-    raiseToPowValue
-} from './helperFunctions'
+import { convertToDays, caseReported, raiseToPowValue } from './helperFunctions';
 
 const covid19ImpactEstimator = (data) => {
 
-    const { reportedCases, periodType, timeToElapse } = data
-    const impart = {}
-    const severeImpart = {}
+    const { reportedCases, periodType, timeToElapse } = data;
+    const impart = {};
+    const severeImpart = {};
 
-    //Challeng 1 /////////////////////////////////////////////////
-    impart.currentlyInfected = caseReported(reportedCases, 10)
-    severeImpact.currentlyInfected = caseReported(reportedCases, 50)
+    impart.currentlyInfected = caseReported(reportedCases, 10);
+    severeImpact.currentlyInfected = caseReported(reportedCases, 50);
 
-    //second question of challenge one
-    const days = convertToDays(periodType, timeToElapse)
+    const days = convertToDays(periodType, timeToElapse);
 
-    const raiseToPow = raiseToPowValue(days)
-    impart.infectionsByRequestedTime = raiseToPow * impart.currentlyInfected
-    severeImpart.infectionsByRequestedTime = raiseToPow * severeImpart.currentlyInfected
-    ///////////////////////////////////////////////////////////////////////////
+    const raiseToPow = raiseToPowValue(days);
+    impart.infectionsByRequestedTime = raiseToPow * impart.currentlyInfected;
+    severeImpart.infectionsByRequestedTime = raiseToPow * severeImpart.currentlyInfected;
+
     return {
         data: { ...data },
         impart,
         severeImpart
 
-    }
-}
+    };
+};
 
 export default covid19ImpactEstimator;
--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,28 +1,26 @@
 
-export const caseReported = (value, mult) => {
-    return value * mult
-}
+export const caseReported = (value, mult) => value * mult;
 
 
 
 export const convertToDays = (periodType, timeToElapse) => {
     switch (periodType) {
         case 'days':
-            return timeToElapse
+            return timeToElapse;
         case 'weeks':
-            const weeks = timeToElapse * 7
-            return weeks
+            const weeks = timeToElapse * 7;
+            return weeks;
         case 'months':
-            const months = timeToElapse * 30
-            return months
+            const months = timeToElapse * 30;
+            return months;
         default:
-            return timeToElapse
+            return timeToElapse;
     }
-}
+};
 
 export const raiseToPowValue = (days) => {
     if (days > 2) {
-        const val = parseInt(days / 2)
-        return Math.pow(2, val)
+        const val = parseInt(days / 2);
+        return Math.pow(2, val);
     }
-}
+};
