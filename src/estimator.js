003728659e0833239a5ba3ebc23155ff2aabcd92
courage173 <courageosemwengie@gmail.com>
2020-04-08 14:50:49 +0100

    completed challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,32 @@
-const covid19ImpactEstimator = (data) => data;
+import {
+    convertToDays,
+    caseReported,
+    raiseToPowValue
+} from './helperFunctions'
+
+const covid19ImpactEstimator = (data) => {
+
+    const { reportedCases, periodType, timeToElapse } = data
+    const impart = {}
+    const severeImpart = {}
+
+    //Challeng 1 /////////////////////////////////////////////////
+    impart.currentlyInfected = caseReported(reportedCases, 10)
+    severeImpact.currentlyInfected = caseReported(reportedCases, 50)
+
+    //second question of challenge one
+    const days = convertToDays(periodType, timeToElapse)
+
+    const raiseToPow = raiseToPowValue(days)
+    impart.infectionsByRequestedTime = raiseToPow * impart.currentlyInfected
+    severeImpart.infectionsByRequestedTime = raiseToPow * severeImpart.currentlyInfected
+    ///////////////////////////////////////////////////////////////////////////
+    return {
+        data: { ...data },
+        impart,
+        severeImpart
+
+    }
+}
 
 export default covid19ImpactEstimator;
