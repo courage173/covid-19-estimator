02a06d8f863a769d03e62bcb826d8b3fd2960159
courage173 <courageosemwengie@gmail.com>
2020-04-08 15:33:19 +0100

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,25 @@
-const covid19ImpactEstimator = (data) => data;
+import { convertToDays, caseReported, raiseToPowValue } from './helperFunctions';
+
+const covid19ImpactEstimator = (data) => {
+
+  const { reportedCases, periodType, timeToElapse } = data;
+  const impact = {};
+  const severeImpact = {};
+
+  impact.currentlyInfected = caseReported(reportedCases, 10);
+  severeImpact.currentlyInfected = caseReported(reportedCases, 50);
+
+  const days = convertToDays(periodType, timeToElapse);
+
+  const raiseToPow = raiseToPowValue(days);
+  impact.infectionsByRequestedTime = raiseToPow * impact.currentlyInfected;
+  severeImpact.infectionsByRequestedTime = raiseToPow * severeImpact.currentlyInfected;
+
+  return {
+    data: { ...data },
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
---
+++ src/helperFunctions.js
@@ -0,0 +1,26 @@
+
+export const caseReported = (value, mult) => value * mult;
+
+export const convertToDays = (periodType, timeToElapse) => {
+  switch (periodType) {
+    case 'days':
+      return timeToElapse;
+    case 'weeks':
+      const weeks = timeToElapse * 7;
+      return weeks;
+    case 'months':
+      const months = timeToElapse * 30;
+      return months;
+    default:
+      return timeToElapse;
+  }
+};
+
+export const raiseToPowValue = (days) => {
+  if (days > 2) {
+    const val = parseInt(days / 2);
+    return Math.pow(2, val);
+  }
+  return val
+
+};
