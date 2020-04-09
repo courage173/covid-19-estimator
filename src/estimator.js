17971ca5206ba8ed4e3d7896cdaf76ce70542e05
courage173 <courageosemwengie@gmail.com>
2020-04-09 07:34:26 +0100

    challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -38,7 +38,7 @@ const covid19ImpactEstimator = (data) => {
 
   impact.casesForVentilatorsByRequestedTime = impact.infectionsByRequestedTime * 0.02;
   severeImpact.casesForVentilatorsByRequestedTime = severeImpact.infectionsByRequestedTime * 0.02;
-  // average daily income
+  // average daily income generator
   const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = data.region;
   const income = avgDailyIncomeInUSD;
   const pop = avgDailyIncomePopulation;
